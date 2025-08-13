92513659cb91334978241d192ab0c9a82488e80a
riodle48 <113567724+riodle48@users.noreply.github.com>
2025-08-13 01:16:43 -0600

    Update mod.rs

--- src/commands/music/mod.rs
+++ src/commands/music/mod.rs
@@ -3,9 +3,11 @@ mod join;
 mod lyrics;
 mod playing;
 mod stop;
+mod tone;            // ➕ add this
 
 pub use disconnect::*;
 pub use join::*;
 pub use lyrics::*;
 pub use playing::*;
 pub use stop::*;
+pub use tone::*;     // ➕ and this
